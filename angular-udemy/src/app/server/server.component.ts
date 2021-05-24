ee16ffa132eb6a0194132e1fa71e19774af8f7c6
MacioBBS <marciobbs@gmail.com>
2021-05-24 14:37:32 -0700

    Create the first component

---
+++ angular-udemy/src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+// Decorator that marks a class as an Angular component and provides configuration metadata that determines how the component should be processed, instantiated, and used at runtime.
+@Component({
+   selector: 'app-server',
+   templateUrl: './server.component.html'
+})
+
+export class ServerComponent {
+
+}
