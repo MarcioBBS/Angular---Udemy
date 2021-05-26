a972df8f1166c75e049bf3fa0bb816765076d624
MacioBBS <marciobbs@gmail.com>
2021-05-25 19:42:49 -0700

    Add comment of how to use selector as an attribute and class

--- angular-udemy/src/app/servers/servers.component.ts
+++ angular-udemy/src/app/servers/servers.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+//   selector: '[app-servers]', // Using selector as Attribute
+//   selector: '.app-servers', // Using selector as Classes
   selector: 'app-servers',
   template: `
    <app-server></app-server>
