1885600a0f1ae2bdac215d63eccc87972827cd11
MacioBBS <marciobbs@gmail.com>
2021-05-27 09:53:23 -0700

    Add Propertie binding

--- angular-udemy/src/app/servers/servers.component.ts
+++ angular-udemy/src/app/servers/servers.component.ts
@@ -4,15 +4,22 @@ import { Component, OnInit } from '@angular/core';
 //   selector: '[app-servers]', // Using selector as Attribute
 //   selector: '.app-servers', // Using selector as Classes
   selector: 'app-servers',
-  template: `
-   <app-server></app-server>
-   <app-server></app-server>
-  `,
+//   template: `
+//    <app-server></app-server>
+//    <app-server></app-server>
+//   `,
+templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
-export class ServersComponent implements OnInit {
 
-  constructor() { }
+export class ServersComponent implements OnInit {
+   public allowNewServer: boolean = false;
+
+  constructor() {
+     setTimeout(()=> {
+        this.allowNewServer = true;
+     }, 3000);
+   }
 
   ngOnInit(): void {
   }
